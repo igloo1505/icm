63f2fe2f99b8978ed952bb395c71c1f9fa21c5f0
Andrew Iglinski <Andrew@igloodevelopment.dev>
2022-12-02 20:08:04 -0600

    initial and final commit

--- hooks/ReduxHooks.ts
+++ hooks/ReduxHooks.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from "../state/store.ts";
+import { RootState, AppDispatch } from "../state/store";
 
 // Use these instead of `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
